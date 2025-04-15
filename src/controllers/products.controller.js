f597dd0332976dcbfb6962bc859f445fd2c1912a
Shahwez Ahmad <shahwez@Shahwezs-MacBook-Air.local>
2025-04-15 12:28:07 +0530

    created model

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,13 +1,14 @@
 
 import path from 'path'
+import ProductModel from '../models/product.model.js'
 
 // export  const getProduct = (req, res) => {
 //     return res.sendFile(path.join(path.resolve(), "src","views","products.html"))
 // }
 
 export default class GetProducts {
-
     static getProducts(req, res)  {
+    console.log(ProductModel.get())
         res.sendFile(path.join(path.resolve(), "src","views","products.html"))
     }
 }
---
+++ src/models/product.model.js
@@ -0,0 +1,38 @@
+
+
+export default  class ProductModel {
+    constructor(id, product_name, desc, imgUrl) {
+        this.id = id
+        this.product_name = product_name
+        this.desc = desc
+        this.imgUrl = imgUrl
+    }
+
+    static get() {
+        return products
+    }
+}
+
+var products = [
+    new ProductModel(
+      1,
+      'Product 1',
+      'Description for Product 1',
+      19.99,
+      'https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg',
+    ),
+    new ProductModel(
+      2,
+      'Product 2',
+      'Description for Product 2',
+      29.99,
+      'https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg',
+    ),
+    new ProductModel(
+      3,
+      'Product 3',
+      'Description for Product 3',
+      39.99,
+      'https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg',
+    ),
+  ]
