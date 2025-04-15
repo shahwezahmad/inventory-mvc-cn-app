5ec6ff496f73819023eaabb793116c0121d75260
Shahwez Ahmad <shahwez@Shahwezs-MacBook-Air.local>
2025-04-16 00:22:51 +0530

    adding valiation while submitting product

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -14,14 +14,29 @@ export default class Products {
     }
 
     static  getddProduct(req, res) {
-        res.render("new-product")
+        res.render("new-product", {errorMessage: null})
     }
 
     static addNewProduct(req, res) {
-        
-
+    
         let {productName, productDesc, productPrice, imgUrl} = req.body
+        let errors = []
+        if(!productName || !productName.trim())  errors.push('Enter Product name')
+        if(!productDesc || !productDesc.trim()) errors.push('Enter product description')
+        if( parseFloat(productPrice) < 1) {
+           
+            errors.push('Price cannot be less than 1')
+        }
+        if(!imgUrl) errors.push('Enter image url')
+            try {
+                 let validUrl = new URL(imgUrl)
+                } catch (error) {
+                errors.push('Enter valid URL')
+            }
 
+            if(errors.length > 0) {
+               return res.render('new-product', {errorMessage: errors[0]})
+            }
 
         ProductModel.add({productName, productDesc, productPrice, imgUrl})
          const products = (ProductModel.get())
